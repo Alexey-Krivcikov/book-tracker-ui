954cce103e37f1c0c875f26ace391ced954b85dd
Aleksey Krivchikov <Krivchikovalexei@yandex.ru>
2026-05-04 19:26:29 +0300

    feature: init project. added prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettierConfig,
+
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off",
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "dist/**",
+    "node_modules/**",
+    "next-env.d.ts",
+    "**/*.min.js",
+    "coverage/**",
+    ".git/**",
+  ]),
+]);
+
+export default eslintConfig;
